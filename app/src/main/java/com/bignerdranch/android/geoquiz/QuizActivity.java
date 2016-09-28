e45af9088a93a5426f5048a1689a1eebb4296445
Martijn Groenier <martijn@groenier.nl>
2016-09-28 16:45:52 +0200

    Added next button to layout and implemented it's functionality, using an array of Questions now, given answers to questions will be checked and user will receive feedback through a Toast message.

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,40 +1,90 @@
 package com.bignerdranch.android.geoquiz;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
-    private Button buttonTrue;
-    private Button buttonFalse;
+    private Button mButtonTrue;
+    private Button mButtonFalse;
+    private Button mButtonNext;
+    private TextView mTextViewQuestion;
+
+    private Question[] mQuestionBank = {
+            new Question(R.string.question_oceans,true),
+            new Question(R.string.question_mideast,false),
+            new Question(R.string.question_africa,false),
+            new Question(R.string.question_americas,true),
+            new Question(R.string.question_asia,true)
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        buttonTrue = (Button) findViewById(R.id.true_button);
-        buttonFalse = (Button) findViewById(R.id.false_button);
+        //save references to the views in variables
+        mButtonTrue = (Button) findViewById(R.id.true_button);
+        mButtonFalse = (Button) findViewById(R.id.false_button);
+        mButtonNext = (Button) findViewById(R.id.next_button);
+        mTextViewQuestion = (TextView) findViewById(R.id.question_text_view);
+
+        updateQuestion();
 
         //setting the onClickListener for the true_button
-        buttonTrue.setOnClickListener(new View.OnClickListener() {
+        mButtonTrue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuizActivity.this, "True-button pressed", Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
 
         //setting the onClickListener for the false_button
-        buttonFalse.setOnClickListener(new View.OnClickListener(){
+        mButtonFalse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuizActivity.this, "False-button pressed", Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
+            }
+        });
+
+        //setting the onClickListener for the next_button
+        mButtonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex+1) % mQuestionBank.length;
+                updateQuestion();
             }
         });
 
     }
+
+    private void updateQuestion() {
+        //get the id of the string in the strings resource file
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        //set the text of the TextView to the content of the question
+        mTextViewQuestion.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+
+        if(userPressedTrue == answerIsTrue){
+            messageResId = R.string.correct_toast;
+        }
+        else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show();
+    }
+
+
 }
